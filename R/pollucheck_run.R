398898d25d093102f9355dbb4733b7dbca05a212
adithirgis <adithi@ilklabs.com>
2021-05-30 17:17:53 +0530

    package details

--- R/pollucheck_run.R
+++ R/pollucheck_run.R
@@ -1,5 +1,11 @@
 #' @title pollucheck: Explore Open-Source Air-Quality Data
-#' @description 
+#' @description This app helps to analyze and visualize open source air
+#' quality data available.  An example dataset is already loaded to help
+#' you walk through all the features of the app.  If you need help with
+#' downloading your own, check the ReadMe file on github.  This app can
+#' process all parameters except Wind Direction which when downloaded at
+#' 1 hour is processed correctly, any other time-resolution is used then
+#' the app will not be process wind direction correctly.
 #' @keywords pollucheck
 #' @examples
 #' \dontrun{
